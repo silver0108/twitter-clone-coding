95410e2b3cd81a50bbeaa1dcf6d2e7d7be636e0c
silver0108 <kk1170@naver.com>
2024-02-26 15:00:42 +0900

    게시글 구현 및 dayjs 사용

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -1,9 +1,50 @@
+import Link from "next/link";
 import styles from "./post.module.css";
+import dayjs from "dayjs";
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
+
+dayjs.locale('ko');
+dayjs.extend(relativeTime);
 
 export default function Post() {
-  return (
-    <main className={styles.main}>
+  const target = {
+    User: {
+      id: 'elonmusk',
+      nickname: 'Elon Musk',
+      image: '/yRsRRjGO.jpg'
+    },
+    content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
+    createAt: new Date(),
+    Images: [],
+  }
 
-    </main>
+  return (
+    <article className={styles.post}>
+      <div className={styles.postWrapper}>
+        <div className={styles.postUserSection}>
+          <Link href={`/${target.User.id}`} className={styles.postUserImage}>
+            <img src={target.User.image} alt={target.User.nickname}/>
+            <div className={styles.postShade}></div>
+          </Link>
+        </div>
+        <div className={styles.postBody}>
+          <div className={styles.postMeta}>
+            <Link href={`/${target.User.id}`}>
+              <span className={styles.postUserName}>{target.User.nickname}</span>
+              &nbsp;
+              <span className={styles.postUserId}>@{target.User.id}</span>
+              &nbsp;
+              .
+              &nbsp;
+            </Link>
+            <span className={styles.postDate}>{dayjs(target.createAt).fromNow(true)}</span>
+          </div>
+          <div>{target.content}</div>
+          <div className={styles.postImageSection}></div>
+          {/* <ActionButtons/> */}
+        </div>
+      </div>
+    </article>
   )
 }
