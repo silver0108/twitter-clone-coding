9a095a95170e2abbe0a6ea1fac04f748e20a0297
silver0108 <kk1170@naver.com>
2024-02-27 18:35:18 +0900

    explore 페이지 구현 및 usePathname 사용

---
+++ src/app/(afterLogin)/_component/RightSearchZone.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import styles from './rightSearchZone.module.css';
+import { usePathname } from "next/navigation";
+import SearchForm from "./SearchForm";
+
+export default function RightSearchZone() {
+  const pathname = usePathname();
+
+  const onChangeAll = () => {};
+  const onChangeFollow = () => {};
+  const onChangeEveryWhere = () => {};
+  const onChangeArround = () => {};
+  
+  if(pathname === '/search') {
+    return null;
+  }
+
+  if(pathname === '/explore') {
+    return (
+      <div>
+        <h3 className={styles.filterTitle}>검색 필터</h3>
+        <div className={styles.filterSection}>
+          <div>
+            <label>사용자</label>
+            <div className={styles.radio}>
+              <div>모든 사용자</div>
+              <input type='radio' name='pf' defaultChecked onChange={onChangeAll} />
+            </div>
+            <div className={styles.radio}>
+              <div>내가 팔로우하는 사람들</div>
+              <input type='radio' name='pf' value="on" onChange={onChangeFollow} />
+            </div>
+          </div>
+          <div>
+            <label>위치</label>
+            <div className={styles.radio}>
+              <div>어디에서나</div>
+              <input type='radio' name='lf' defaultChecked onChange={onChangeEveryWhere} />
+            </div>
+            <div className={styles.radio}>
+              <div>현 위치 주변</div>
+              <input type='radio' name='lf' value="on" onChange={onChangeArround} />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div style={{ marginBottom: 60, width: 'inherit' }}>
+      <SearchForm/>
+    </div>
+  )
+}
--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,7 +1,25 @@
+import SearchForm from '../_component/SearchForm';
+import Trend from '../_component/Trend';
+import styles from './explore.module.css';
+
 export default function Explore() {
   return (
-    <main>
-      탐색
+    <main className={styles.main}>
+      <div className={styles.formZone}>
+        <SearchForm/>
+      </div>
+      <div className={styles.trend}>
+        <Trend/>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
     </main>
   )
 }
