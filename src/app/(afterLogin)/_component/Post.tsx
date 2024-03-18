c861508e97c125d735a0889b06e377eccb079e78
silver0108 <kk1170@naver.com>
2024-03-18 15:14:48 +0900

    다중 이미지 구역 및 반응형 구현

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -1,74 +1,66 @@
+import style from './post.module.css';
 import Link from "next/link";
-import styles from "./post.module.css";
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/ko';
-import ActionButtons from "./ActionButtons";
-import Image from "next/image";
-import PostArticle from "./PostArticle";
+import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
+import PostArticle from "@/app/(afterLogin)/_component/PostArticle";
 import { faker } from '@faker-js/faker';
+import PostImages from "@/app/(afterLogin)/_component/PostImages";
 
 dayjs.locale('ko');
-dayjs.extend(relativeTime);
+dayjs.extend(relativeTime)
 
 type Props = {
   noImage?: boolean
 }
-
-export default function Post( {noImage} : Props) {
+export default function Post({ noImage }: Props) {
   const target = {
     postId: 1,
     User: {
       id: 'elonmusk',
       nickname: 'Elon Musk',
-      image: '/yRsRRjGO.jpg'
+      image: '/yRsRRjGO.jpg',
     },
     content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
     createdAt: new Date(),
     Images: [] as any[],
   }
-
   if (Math.random() > 0.5 && !noImage) {
     target.Images.push(
       { imageId: 1, link: faker.image.urlLoremFlickr() },
-      // { imageId: 2, link: faker.image.urlLoremFlickr() },
-      // { imageId: 3, link: faker.image.urlLoremFlickr() },
-      // { imageId: 4, link: faker.image.urlLoremFlickr() }
+      { imageId: 2, link: faker.image.urlLoremFlickr() },
+      { imageId: 3, link: faker.image.urlLoremFlickr() },
+      { imageId: 4, link: faker.image.urlLoremFlickr() },
     )
   }
 
   return (
     <PostArticle post={target}>
-      <div className={styles.postWrapper}>
-        <div className={styles.postUserSection}>
-          <Link href={`/${target.User.id}`} className={styles.postUserImage}>
-            <Image src={target.User.image} alt={target.User.nickname} width={40} height={40} />
-            <div className={styles.postShade}></div>
+      <div className={style.postWrapper}>
+        <div className={style.postUserSection}>
+          <Link href={`/${target.User.id}`} className={style.postUserImage}>
+            <img src={target.User.image} alt={target.User.nickname} />
+            <div className={style.postShade} />
           </Link>
         </div>
-        <div className={styles.postBody}>
-          <div className={styles.postMeta}>
+        <div className={style.postBody}>
+          <div className={style.postMeta}>
             <Link href={`/${target.User.id}`}>
-              <span className={styles.postUserName}>{target.User.nickname}</span>
+              <span className={style.postUserName}>{target.User.nickname}</span>
               &nbsp;
-              <span className={styles.postUserId}>@{target.User.id}</span>
+              <span className={style.postUserId}>@{target.User.id}</span>
               &nbsp;
-              .
+              ·
               &nbsp;
             </Link>
-            <span className={styles.postDate}>{dayjs(target.createdAt).fromNow(true)}</span>
+            <span className={style.postDate}>{dayjs(target.createdAt).fromNow(true)}</span>
           </div>
           <div>{target.content}</div>
-          <div className={styles.postImageSection}>
-            { target.Images && target.Images.length > 0 && (
-              <Link href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
-                className={styles.postImageSection}
-              >
-                <img src={target.Images[0]?.link} alt=""></img>
-              </Link>
-            )}
+          <div>
+            <PostImages post={target} />
           </div>
-          <ActionButtons/>
+          <ActionButtons />
         </div>
       </div>
     </PostArticle>
