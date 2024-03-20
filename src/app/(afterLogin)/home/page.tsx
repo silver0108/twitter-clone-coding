30aaee368f5c7af340d8dea7f93c81f04456b85a
silver0108 <kk1170@naver.com>
2024-03-21 02:54:16 +0900

    클라이언드에서 react-query 사용

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,43 +1,26 @@
 import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
-import Post from "../_component/Post";
 import PostForm from "./_component/PostForm";
 import Tab from "./_component/Tab";
 import TabProvider from "./_component/TabProvider";
 import styles from "./home.module.css";
-
-async function getPostRecommends() {
-  const res = await fetch('http://localhost:9090/api/postRecommends', {
-    next: {
-      tags: ['posts', 'recommends'], // next에서 지원, 캐시 초기화를 위한 태그
-    },
-    cache: 'no-store', // 캐싱 X
-  });
-
-  if(!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-
-  return res.json();
-}
+import { getPostRecommends } from "./_lib/getPostRecommends";
+import PostRecommends from "./_component/PostRecommends";
 
 export default async function Home() {
+  // react-query ssr
   const queryClient = new QueryClient();
-  // queryKey를 갖고 있을 때, queryFn 실행해서 값을 가져오기
-  await queryClient.prefetchQuery({queryKey: ['post', 'recommends'], queryFn: getPostRecommends});
+  // queryKey를 갖고 있을 때, queryFn 실행해서 데이터를 가져오기
+  await queryClient.prefetchQuery({queryKey: ['posts', 'recommends'], queryFn: getPostRecommends});
   const dehydratedState = dehydrate(queryClient);
 
   return (
     <main className={styles.main}>
-      {/* 서버에서 가져온 데이터를 클라이언트에 맞게 hydrate */}
+      {/* 서버에서 가져온 데이터를 클라이언트에 맞는 react-query로 만듦 */}
       <HydrationBoundary state={dehydratedState}>
         <TabProvider>
           <Tab />
           <PostForm />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
+          <PostRecommends />
         </TabProvider>
       </HydrationBoundary>
     </main>
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,30 @@
 import { http, HttpResponse } from 'msw';
+import { faker } from "@faker-js/faker";
+
+// 지난 주 부터 오늘까지 랜덤으로 날짜 하나 골라주는 함수
+function generateDate() {
+  const lastWeek = new Date(Date.now());
+  lastWeek.setDate(lastWeek.getDate() - 7);
+  return faker.date.between({
+    from: lastWeek,
+    to: Date.now(),
+  });
+}
+
+const User = [
+  { id: 'elonmusk', nickname: 'Elon Musk', image: '/yRsRRjGO.jpg' },
+  { id: 'zerohch0', nickname: '제로초', image: '/5Udwvqim.jpg' },
+  { id: 'silver', nickname: '실버', image: faker.image.avatar() },
+]
+const Posts = [];
+const delay = (ms: number) => new Promise((res) => {
+  setTimeout(res, ms);
+})
 
 export const handlers = [
   http.post('/api/login', () => {
     console.log('로그인');
-    return HttpResponse.json({
+    return HttpResponse.json({ // json으로 보낼 때
       userId: 1,
       nickname: '실버',
       id: 'silver',
@@ -16,7 +37,7 @@ export const handlers = [
   }),
   http.post('/api/logout', () => {
     console.log('로그아웃');
-    return new HttpResponse(null, {
+    return new HttpResponse(null, { // 보낼 데이터가 없을 때 new HttpResponse 주로 사용
       headers: {
         'Set-Cookie': 'connect.sid=;HttpOnly;Path=/;Max-Age=0' // 쿠키 지우기
       }
@@ -27,10 +48,66 @@ export const handlers = [
     // return HttpResponse.text(JSON.stringify('user_exists'), {
     //   status: 403,
     // })
-    return HttpResponse.text(JSON.stringify('ok'), {
+    return HttpResponse.text(JSON.stringify('ok'), { // 텍스트로 보낼 때
       headers: {
         'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/;Max-Age=0'
       }
     })
+  }),
+  http.get('/api/postRecommends', async ({ request }) => {
+    await delay(3000);
+    const url = new URL(request.url)
+    const cursor = parseInt(url.searchParams.get('cursor') as string) || 0
+    return HttpResponse.json(
+      [
+        {
+          postId: cursor + 1,
+          User: User[0],
+          content: `${cursor + 1} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+          createdAt: generateDate(),
+        },
+        {
+          postId: cursor + 2,
+          User: User[0],
+          content: `${cursor + 2} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [
+            { imageId: 1, link: faker.image.urlLoremFlickr() },
+            { imageId: 2, link: faker.image.urlLoremFlickr() },
+          ],
+          createdAt: generateDate(),
+        },
+        {
+          postId: cursor + 3,
+          User: User[0],
+          content: `${cursor + 3} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [],
+          createdAt: generateDate(),
+        },
+        {
+          postId: cursor + 4,
+          User: User[0],
+          content: `${cursor + 4} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [
+            { imageId: 1, link: faker.image.urlLoremFlickr() },
+            { imageId: 2, link: faker.image.urlLoremFlickr() },
+            { imageId: 3, link: faker.image.urlLoremFlickr() },
+            { imageId: 4, link: faker.image.urlLoremFlickr() },
+          ],
+          createdAt: generateDate(),
+        },
+        {
+          postId: cursor + 5,
+          User: User[0],
+          content: `${cursor + 5} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [
+            { imageId: 1, link: faker.image.urlLoremFlickr() },
+            { imageId: 2, link: faker.image.urlLoremFlickr() },
+            { imageId: 3, link: faker.image.urlLoremFlickr() },
+          ],
+          createdAt: generateDate(),
+        },
+      ]
+    )
   }),
 ]
